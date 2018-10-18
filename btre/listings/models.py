532b415e6a9674612fdcb0337b3f382d7324c647
isaaczhou <isaaczhou85@gmail.com>
2018-10-18 13:20:08 -0400

    added listings and realtors models

--- btre/listings/models.py
+++ btre/listings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from realtors import Realtor
+from realtors.models import Realtor
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
@@ -24,7 +24,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     photo_6 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default=datetime.now(), blank=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
     
     def __str__(self):
         return self.title
