ae5619cb5f0479c175f4d8f98ea190c3d625b281
isaaczhou <isaaczhou85@gmail.com>
2018-10-19 16:27:02 -0400

    updated

--- btre_proj/btre/settings.py
+++ btre_proj/btre/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig', 'listings.apps.ListingsConfig',
-    'realtors.apps.RealtorsConfig', 'django.contrib.admin',
+    'realtors.apps.RealtorsConfig', 'accounts.apps.AccountsConfig','django.contrib.admin',
     'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages',
     'django.contrib.staticfiles', 'django.contrib.humanize'
@@ -123,3 +123,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
