d031200609dba5e32bbf6cc6aca71866703dc6a2
isaaczhou <isaaczhou85@gmail.com>
2018-10-24 16:01:30 -0400

    created and defined contacts

--- btre_proj/btre/settings.py
+++ btre_proj/btre/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig', 'listings.apps.ListingsConfig',
-    'realtors.apps.RealtorsConfig', 'accounts.apps.AccountsConfig','django.contrib.admin',
+    'realtors.apps.RealtorsConfig', 'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig','django.contrib.admin',
     'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages',
     'django.contrib.staticfiles', 'django.contrib.humanize'
