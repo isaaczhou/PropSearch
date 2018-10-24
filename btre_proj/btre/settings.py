a54c4f1a53ffd7f368b7f6b1609369c8db327158
isaaczhou <isaaczhou85@gmail.com>
2018-10-24 18:04:28 -0400

    finished inquiry

--- btre_proj/btre/settings.py
+++ btre_proj/btre/settings.py
@@ -130,3 +130,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Config
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "isaac20johnson@gmail.com"
+EMAIL_HOST_PASSWORD = "Isaac800Isaac"
+EMAIL_USE_TLS = True
