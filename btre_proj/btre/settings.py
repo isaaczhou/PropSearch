b1fa18fb29285f5104414d3c69c45631a06936a8
isaaczhou <isaaczhou85@gmail.com>
2018-10-24 19:07:01 -0400

    added local settings

--- btre_proj/btre/settings.py
+++ btre_proj/btre/settings.py
@@ -137,3 +137,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "isaac20johnson@gmail.com"
 EMAIL_HOST_PASSWORD = "Isaac800Isaac"
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
